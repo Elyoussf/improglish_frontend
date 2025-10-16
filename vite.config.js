fb3b895f8db97a7ada460d783a353abd8e3096e0
elyoussfiihaamza-bit <elyoussfiihaamza@gmail.com>
2025-10-16 17:48:23 +0100

    __-__

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['4c819f983cab.ngrok-free.app'], // 👈 Add your ngrok domain here
+
     host: true,  // Allows access from network/IP
   },
 })
